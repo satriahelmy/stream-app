10e5d560df55b3708283c754685644ac712c2548
satriahelmy <helmysmp@gmail.com>
2023-07-29 09:25:13 +0700

    updated movie

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Models\Movie;
+use Illuminate\Support\Facades\Storage;
 
 class MovieController extends Controller
 {
@@ -15,9 +16,15 @@ class MovieController extends Controller
         return view('admin.movies',['movies'=>$movies]);
     }
 
-   public function create()
+    public function create()
     {
         return view('admin.movie-create');
+    }
+
+    public function edit($id)
+    {
+        $movie = Movie::find($id);
+        return view("admin.movie-edit",['movie'=>$movie]);
     }
 
     public function store(Request $request)
@@ -49,9 +56,53 @@ class MovieController extends Controller
 
         $data['small_thumbnail'] = $originalSmallThumbanilName;
         $data['large_thumbnail'] = $originalLargeThumbanilName;
-        
+
         Movie::create($data);
 
         return redirect()->route('admin.movie')->with('success','Movie Created');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->except('_token');
+        $request->validate([
+            'title' => 'required|string',
+            'small_thumbnail' => 'image|mimes:jpeg,jpg,png',
+            'large_thumbnail' => 'image|mimes:jpeg,jpg,png',
+            'trailer' => 'required|url',
+            'movie' => 'required|url',
+            'casts' => 'required|string',
+            'categories' => 'required|string',
+            'release_date' => 'required|string',
+            'about' => 'required|string',
+            'short_about' => 'required|string',
+            'duration' => 'required|string',
+            'featured' => 'required',
+        ]);
+
+        $movie=Movie::find($id);
+
+        if($request->small_thumbnail)
+        {
+            $smallThumbnail = $request->small_thumbnail;
+            $originalSmallThumbanilName = Str::random(10).$smallThumbnail->getClientOriginalName();
+            $smallThumbnail->storeAs('public/thumbnail',$originalSmallThumbanilName);
+            $data['small_thumbnail'] = $originalSmallThumbanilName;
+
+            Storage::delete('public/thumbnail/'.$movie->small_thumbnail);
+        }
+
+        if($request->small_thumbnail)
+        {
+            $largeThumbnail = $request->large_thumbnail;
+            $originalLargeThumbanilName = Str::random(10).$largeThumbnail->getClientOriginalName();
+            $largeThumbnail->storeAs('public/thumbnail',$originalLargeThumbanilName);
+            $data['large_thumbnail'] = $originalLargeThumbanilName;
+
+            Storage::delete('public/thumbnail/'.$movie->large_thumbnail);
+        }
+
+        $movie->update($data);
+        return redirect()->route('admin.movie')->with('success','Movie Updated');
     }
 }
--- resources/views/admin/movies.blade.php
+++ resources/views/admin/movies.blade.php
@@ -9,7 +9,7 @@
         <div class="card-header">
           <h3 class="card-title">Movies</h3>
         </div>
-        
+
         <div class="card-body">
           <div class="row">
             <div class="col-md-12">
@@ -49,7 +49,7 @@
                       </td>
                       <td>{{ $movie->categories }}</td>
                       <td>{{ $movie->casts }}</td>
-                      <td><a href="" class="btn btn-secondary"><i class="fas fa-edit"></i><a></td>
+                      <td><a href="{{ route('admin.movie.edit', $movie->id) }}" class="btn btn-secondary"><i class="fas fa-edit"></i><a></td>
                     </tr>
                   @endforeach
                 </tbody>
